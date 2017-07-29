61ecd0e46fbda4dc0ff96b9685405135eedfd0f3
Andrew Kadesky <amkadesky@aol.com>
2017-07-29 18:04:27 -0500

    Application now counts newly created messages from twitch chat. counter resets after 100 messages are counted. twitch ui load an initial 50 messages.

--- app.js
+++ app.js
@@ -4,66 +4,81 @@ var MutationObserver = window.MutationObserver || window.WebKitMutationObserver 
 
 var COUNT = 0;
 
-var config = {attributes: false, childList: true, characterData: true};
-
+var config = { attributes: false, childList: true, characterData: true };
 var htmlBody = $("body")[0];
-var chatLoadedObserver = new MutationObserver(function(mutation){
-	console.log("new mutationobserver loaded");
-	mutation.forEach(function(mutation){
-		var chatSelector = $(".chat-lines");
-		console.log("chat selected");
-		if (chatSelector.length > 0){
-				var target = chatSelector[0];
-				console.log("found twitch chat");
 
-				chatObserver.observer(target, config);
-				observer.disconnect();
-			}
-		});
-});
 
-var chatObserver = new MutationObserver(function(mutation){
-	mutation.forEach(function(mutation){
-		mutation.addedNodes.forEach(function(addedNode){
-			var chatMessage = $(addedNode);
-			if(!chatMessage.is(".chat-line", ".message-line")){
-				console.log("not a chat message.");
-				return;
-			}
-			var messageElement = chatMessage.children('.message');
-			countMsgHTML(messageElement);
-		});
+var countMsgHTML = function(msgHTML) {
+    COUNT += 1;
+    console.log("found a message");
+    if(COUNT == 100){
+    	COUNT = 0;
+    }
+    console.log(COUNT);
+};
+
+function chatObserver() {
+	console.log("using chatObserver");
+	return new MutationObserver(function(mutations){
+	    mutations.forEach(function(mutation) {
+	        mutation.addedNodes.forEach(function(addedNode) {
+	            var chatMessage = $(addedNode);
+	            if (!chatMessage.is(".chat-line", ".message-line")) {
+	                return;
+	            }
+	            var messageElement = chatMessage.children('.message');
+	            countMsgHTML(messageElement);
+	        });
+	    });
 	});
+};
+
+var chatSearcher = chatObserver();
+
+
+var chatLoadedObserver = new MutationObserver(function(mutation) {
+    console.log("new mutationobserver loaded");
+    mutation.forEach(function(mutation) {
+        var chatSelector = $(".chat-lines");
+        console.log("chat selected");
+        if (chatSelector.length > 0) {
+            var target = chatSelector[0];
+            console.log("found twitch chat");
+
+            chatSearcher.observe(target, config);
+            observer.disconnect();
+        }
+    });
 });
 
-var countMsgHTML = function(msgHTML){
-	COUNT += 1;
-	console.log("found a message");
-	console.log(COUNT);
-}
+
 
 // chatObserver.disconnect();
 
 //on load ask user for permission for Notification APi to access their information.
- function notifyMe(){
-	console.log("using notifyMe")
-	if(!("Notification" in window)){
-		alert("This browser does not support desktop notification")
-	}
+function notifyMe() {
+    console.log("using notifyMe")
 
-	else if(Notification.permission === "granted"){
-		var notification = new Notification("Hi there!");
-		chatLoadedObserver.observe(htmlBody, config);
-	}
 
-	else if(Notification.permission !== "denied"){
-		Notification.requestPermission(function(permission){
-			if(permission === "granted"){
-				var notification = new Notification("Hi there");
-				chatLoadedObserver.observe(htmlBody, config);
-				}
-			})
-		};
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification")
+        console.log("browser not supported");
+    } else if (Notification.permission === "granted") {
+        var notification = new Notification("Hi there!");
+        chatLoadedObserver.observe(htmlBody, config);
+        console.log("permission was already granted");
+    } else if (Notification.permission !== "denied") {
+        Notification.requestPermission(function(permission) {
+            if (permission === "granted") {
+                var notification = new Notification("Hi there");
+                chatLoadedObserver.observe(htmlBody, config);
+                console.log("permission was granted");
+            }
+        });
+    }
+    else{
+    	console.log("statements aren't working");
+    }
 }
 
 notifyMe();
