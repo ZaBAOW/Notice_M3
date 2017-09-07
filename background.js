dba814537665c60c84b96bdc957aa69defba4563
Andrew Kadesky <amkadesky@aol.com>
2017-09-07 13:59:04 -0500

    icon and usernames now show up on the notifications.

--- background.js
+++ background.js
@@ -5,16 +5,31 @@
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MutationObserver;
 
 
-// var target = $(".message")[0];
-
 window.ENABLED = true;
 var threshold = 0;
 var count = 0;
 var RESULT = count;
 window.EXECUTED = false;
+
 var appIcon = new Image();
-appIcon.src = '/notice-icon.png';
-var channelIcon = document.getElementsByClassName("cn-bar__avatar");
+appIcon.src = 'http://i.imgur.com/KS6rJQU.png';
+var channelIcon;
+var channelName;
+
+var getChannelInfo = setInterval(function(){
+    console.log("channel Loaded!");
+    channelIcon = $('.cn-bar__avatar').attr('src');
+    channelName = $('.cn-bar__displayname').html();
+}, 5000);
+
+// var channelIcon.onload = $("cn-bar__avatar").attr("src");
+
+// var channelIcon = $('cn-bar__avatar').attr('src');
+
+// $(document).on('load', function(){
+//     var curProfile = $('cn-bar__avatar').attr('src');
+//     channelIcon = curProfile;
+// });
 
 var config = { attributes: true, childList: true, characterData: true };
 var htmlBody = $("body")[0];
@@ -58,7 +73,7 @@ var setThreshold = setInterval(function(){
 		EXECUTED = true;
 		threshold = count * 0.95;
         localStorage.channelThreshold = threshold;     
-		spawnNotification("threshold was set.", appIcon, "Notice_M3");
+		spawnTwitchNotification("threshold was set.", channelIcon, channelName);
 	}
 }, 59 * 1000);
 
@@ -74,13 +89,29 @@ var noticeMe = setInterval(function(){
         console.log("Notify_M3 is off");
     }
 	else if(count >= threshold){
-        spawnNotification("NOTICE ME!!!", appIcon, "Notice_M3");
+        spawnTwitchNotification("NOTICE ME!!!", channelIcon, channelName);
         // var n = new Notification("NOTICE ME!!!", {icon: "notice-icon.png"});
 	}
 	count = 0;
 }, 60 * 1000);
 
-function spawnNotification(theBody, theIcon, theTitle){
+function spawnAppNotification(theBody, theIcon, theTitle){
+    var options = {
+        body: theBody,
+        icon: theIcon.src
+    }
+
+    var n = new Notification(theTitle, options);
+    setTimeout(n.close.bind(n), 5000);
+    n.onclick = function(event){
+        event.preventDefault();
+        console.log("notification clicked");
+        window.focus();
+        n.close();
+    }
+}
+
+function spawnTwitchNotification(theBody, theIcon, theTitle){
     var options = {
         body: theBody,
         icon: theIcon
@@ -96,6 +127,7 @@ function spawnNotification(theBody, theIcon, theTitle){
     }
 }
 
+
 var checkEnable = setInterval(function(){
     console.log(EXECUTED);
 }, 5000);
@@ -106,7 +138,7 @@ chrome.runtime.onMessage.addListener(
             "from a content script:" + sender.tab.url :
             "from the extension");
         if (request.executed === false){
-            spawnNotification("threshold will reset.", appIcon, "Notice_M3");
+            spawnTwitchNotification("threshold will reset.", channelIcon, channelName);
             window.EXECUTED = false;
             sendResponse({recieved: "message recieved"});
             return true;
@@ -115,7 +147,7 @@ chrome.runtime.onMessage.addListener(
         else if(request.enabled === false){
             window.ENABLED = false;
             sendResponse({recieved: "will disable Notify_M3"});
-            spawnNotification("Notify_M3 has been Disabled.", appIcon, "Notice_M3");
+            spawnAppNotification("Notify_M3 has been Disabled.", appIcon, "Notice_M3");
             localStorage.enableState = window.ENABLED;
             return true;
         }
@@ -123,7 +155,7 @@ chrome.runtime.onMessage.addListener(
         else if(request.enabled === true){
             window.ENABLED = true;
             sendResponse({recieved: "will enable Notify_M3"});
-            spawnNotification("Notify_M3 has been Enabled.", appIcon, "Notice_M3");
+            spawnAppNotification("Notify_M3 has been Enabled.", appIcon, "Notice_M3");
             localStorage.enableState = window.ENABLED;
             return true;
         }
@@ -183,14 +215,14 @@ function notifyMe() {
         console.log("browser not supported");
     } else if (Notification.permission === "granted") {
         // var notification = new Notification("Hi there!");
-        spawnNotification("Hi There!", channelIcon, "Notice_M3");
+        spawnAppNotification("Hi There!", appIcon, "Notice_M3");
         chatLoadedObserver.observe(htmlBody, config);
         console.log("permission was already granted");
     } else if (Notification.permission !== "denied") {
         Notification.requestPermission(function(permission) {
             if (permission === "granted") {
                 // var notification = new Notification("Hi there");
-                spawnNotification("Hi There!", channelIcon, "Notice_M3");
+                spawnAppNotification("Hi There!", appIcon, "Notice_M3");
                 chatLoadedObserver.observe(htmlBody, config);
                 console.log("permission was granted");
             }
